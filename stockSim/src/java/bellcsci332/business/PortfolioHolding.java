d45f3bff94f58d4a01a4d8d62e9321e1c285cd55
Matthew Bell <bellmj@g.cofc.edu>
2017-11-23 20:00:57 -0500

    added another bean

--- stockSim/src/java/bellcsci332/business/PortfolioHolding.java
+++ stockSim/src/java/bellcsci332/business/PortfolioHolding.java
@@ -5,10 +5,59 @@
  */
 package bellcsci332.business;
 
+import java.sql.Date;
+
 /**
  *
  * @author Matthew Bell
  */
 public class PortfolioHolding {
+    private Date timeStamp;
+    private int quantityHeld;
+    private String ownerEmail;
+    private String symbol;
+
+    public PortfolioHolding() {
+    }
+
+    public PortfolioHolding(Date timeStamp, int quantityHeld, String ownerEmail, String symbol) {
+        this.timeStamp = timeStamp;
+        this.quantityHeld = quantityHeld;
+        this.ownerEmail = ownerEmail;
+        this.symbol = symbol;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Date timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public int getQuantityHeld() {
+        return quantityHeld;
+    }
+
+    public void setQuantityHeld(int quantityHeld) {
+        this.quantityHeld = quantityHeld;
+    }
+
+    public String getOwnerEmail() {
+        return ownerEmail;
+    }
+
+    public void setOwnerEmail(String ownerEmail) {
+        this.ownerEmail = ownerEmail;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+    
     
 }
